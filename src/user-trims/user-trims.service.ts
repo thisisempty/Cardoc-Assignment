97f58fb958401c7757c2ef6a9110878581e39e23
thisisempty <thisisemptyyy@gmail.com>
2021-11-28 22:26:33 +0900

    [CREATE] 사용자 소유의 자동차 정보 저장 service 생성

--- src/user-trims/user-trims.service.ts
+++ src/user-trims/user-trims.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+
+import { BadRequestException, 
+         Injectable, 
+         InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository }             from '@nestjs/typeorm';
+import { AuthService }                  from 'src/auth/auth.service';
+
+import { CreateUserTrimDto } from './dto/create-user-trim.dto';
+import { UserTrim }          from './entity/user-trim.entity';
 
 @Injectable()
-export class UserTrimsService {}
+export class UserTrimsService {
+    constructor(
+        @InjectRepository(UserTrim) private userTrimRepository: Repository<UserTrim>,
+        private authServive: AuthService
+    ){}
+
+    async create(
+        createUserTrimDto: CreateUserTrimDto,
+        userId: string
+    ): Promise<UserTrim>{
+        const user = await this.authServive.getUserById(userId);
+
+        if (!user) {
+            throw new BadRequestException("DOES_NOT_EXIST");
+        }
+
+        try {
+            const userTrim = this.userTrimRepository.create({
+                trimId: createUserTrimDto.trimId,
+                user
+            })
+
+            return await this.userTrimRepository.save(userTrim);
+        } catch (error) {
+            throw new InternalServerErrorException(error.message);
+        }
+    }
+}
